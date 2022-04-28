82e3c3592ef86b372d0c8e4fbee52f757fab6ba3
BrainTvinki <101339439+BrainTvinki@users.noreply.github.com>
2022-04-28 22:15:33 +0600

    Update CoffeeMachine.java

--- Coffee Machine/CoffeeMachine.java
+++ Coffee Machine/CoffeeMachine.java
@@ -35,82 +35,12 @@ public class CoffeeMachine {
 
     public static void main(String[] args) {
 
-/*
-        startingToMakeACoffee();
-        grindingCoffeeBeans();
-        boilingWater();
-        mixingBoiledWaterWithCrushedCoffeeBeans();
-        pouringCoffeeIntoTheCup();
-        pouringSomeMilkIntoTheCup();
-        coffeeIsReady();
-        calculateIngredients();
- */
+
         while (!finishProgramm) {
         displayMenu();
         }
-
-   /*     theCoffeeMachineHas();
-        fillTheMachine();
-        howManyCupsMachineCanProduce();
-        howManyCupsUserCanGet();
-
-    */
     }
-    /*
-    private static void startingToMakeACoffee () {
-       System.out.println("Starting to make a coffee");
-    }
-    private static void grindingCoffeeBeans () {
-        System.out.println("Grinding coffee beans");
-    }
-    private static void boilingWater () {
-        System.out.println("Boiling water");
-    }
-    private static void mixingBoiledWaterWithCrushedCoffeeBeans () {
-        System.out.println("Mixing boiled water with crushed coffee beans");
-    }
-    private static void pouringCoffeeIntoTheCup () {
-        System.out.println("Pouring coffee into the cup");
-    }
-    private static void pouringSomeMilkIntoTheCup () {
-        System.out.println("Pouring some milk into the cup");
-    }
-    private static void coffeeIsReady () {
-        System.out.println("Coffee is ready!");
-    }
-    private static void calculateIngredients () {
-        System.out.println("Write how many cups of coffee you will need: ");
-        Scanner scanner = new Scanner (System.in);
-        int quantityOfCupsUserWillNeed = scanner.nextInt();
-        System.out.printf("For %d cups of coffee you will need:\n" +
-                "%d ml of water\n" +
-                "%d ml of milk\n" +
-                "%d g of coffee beans", quantityOfCupsUserWillNeed,
-                quantityOfCupsUserWillNeed * waterForOneCup,
-                quantityOfCupsUserWillNeed * milkForOneCup,
-                quantityOfCupsUserWillNeed * beansForOneCup);
-    }
-
-    private static void howManyCupsUserCanGet () {
-        Scanner scanner = new Scanner (System.in);
-        System.out.println("Write how many cups of coffee you will need: ");
-        int howManyCupsUserNeed = scanner.nextInt();
-        if (cupsCanBeProduced > howManyCupsUserNeed) {
-            System.out.printf("Yes, I can make that amount of coffee (and even %d more than that)\n",
-                    cupsCanBeProduced - howManyCupsUserNeed);
-        } else if (cupsCanBeProduced == howManyCupsUserNeed) {
-            System.out.println("Yes, I can make that amount of coffee");
-        } else {
-            System.out.printf("No, I can make only %d cup(s) of coffee", cupsCanBeProduced);
-        }
-    }
-    private static void howManyCupsMachineCanProduce() {
-        int waterForCups = waterInMachine / waterForOneCup;
-        int milkForCups = milkInMachine / milkForOneCup;
-        int beansForCups = beansInMachine / beansForOneCup;
-        cupsCanBeProduced = Math.min(Math.min(waterForCups, milkForCups),beansForCups);
-    }
-    */
+   
     private static void theCoffeeMachineHas() {
         System.out.printf("The coffee machine has: \n" +
                         "%d ml of water\n" +
